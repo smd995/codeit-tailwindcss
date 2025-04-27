17fcb2ed3810bc43e0af9c821e753be05778b09b
smd995 <smd99540@gmail.com>
2025-04-27 14:45:49 +0900

    feat:button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+import Button from "@/components/button";
+import { useState } from "react";
+
 export default function Home() {
-  return <div className="bg-blue-600">안녕 tailwindcss plugin</div>;
+  const [isButton, setIsButton] = useState<string>("");
+
+  const handleClick = () => {
+    setIsButton(() => {
+      return "나는 버튼이야!";
+    });
+  };
+
+  return (
+    <>
+      <Button onClick={handleClick}>버튼을 클릭하세요 {isButton}</Button>
+    </>
+  );
 }
---
+++ src/components/button/index.tsx
@@ -0,0 +1,64 @@
+import clsx from "clsx";
+
+const BASE_STYLES =
+  "rounded font-medium transition-colors focus:ring-2 focus:ring-offset-2 focus:outline-none";
+
+const VARIANT_STYLES = {
+  primary: "bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500",
+  secondary: "bg-gray-200 text-gray-900 hover:bg-gray-300 focus:ring-gray-500",
+  outline:
+    "border border-blue-600 text-blue-600 hover:bg-blue-50 focus:ring-blue-500",
+  ghost: "focus:ring-blue500 text-blue-600 hover:bg-blue-50",
+  link: "p-0 text-blue-600 hover:underline focus:ring-blue-500",
+};
+
+const SIZE_STYLES = {
+  sm: "px-3 py-1 text-sm",
+  md: "px-4 py-2 text-base",
+  lg: "px-6 py-3 text-lg",
+};
+
+const DISABLED_STYLES = "cursor-not-allowed opacity-50";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  variant?: "primary" | "secondary" | "outline" | "ghost" | "link";
+  size?: "sm" | "md" | "lg";
+  fullwidth?: boolean;
+  disabled?: boolean;
+  className?: string;
+  onClick: () => void;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({
+  children,
+  variant = "primary",
+  size = "md",
+  fullwidth = false,
+  disabled = false,
+  className = "",
+  onClick,
+  ...props
+}: ButtonProps) => {
+  const buttonClasses = clsx(
+    BASE_STYLES,
+    VARIANT_STYLES[variant],
+    SIZE_STYLES[size],
+    fullwidth && "w-full",
+    disabled && DISABLED_STYLES,
+    className,
+  );
+
+  return (
+    <button
+      className={clsx(buttonClasses)}
+      disabled={disabled}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
